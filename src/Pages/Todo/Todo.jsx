25c86e25e41bba7c1a523a68ede820c1f16dc435
Maxime115 <leclercqmaximedu62@gmail.com>
2023-10-20 15:27:33 +0200

    Test Backup

--- src/Pages/Todo/Todo.jsx
+++ src/Pages/Todo/Todo.jsx
@@ -1,34 +1,44 @@
 import { useEffect, useState } from "react";
 
 const Todo = () => {
-    const [todolist, setTodoList] = useState([])
-    
+    const [todoList, setTodoList] = useState([{Nom_Personnage:""}])
+
     useEffect( () => {
-        async function getTodo() {
-            try {
-                const response = await fetch("http://localhost:8000/getTodos")
+        async function getTodo(){
+            try{
+                const response = await fetch("http://localhost:8000/getPerso");
                 if (response.ok){
                     const todos = await response.json();
+                    console.log(todos);
                     setTodoList(todos)
-                } else {
+                } else{
                     console.log("Il y a eu une erreur");
-        
                 }
-
-            } catch (error) {
+            } 
+            catch (error){
                 console.log(error);
             }
         }
         getTodo();
     }, [])
+
     return(
         <>
-
-        {todolist.map((personnage) =>
-            <p classkey={personnage.IdPersonnage}>{personnage.Nom_Personngage}</p>
+        {console.log(todoList)}
+        <p>NOM PERSO : {todoList[0].Nom_Personnage}</p>
+        <br></br>
+        <p>AGE : {todoList[0].Age}</p>
         
-        )}
+        
+        {/* {todoList.map((todos) => 
+           { return <div> <p key={todos.id}>{todos.Nom_Personnage} </p></div>}
+            
+        )} */}
+        <p></p>
+        
+        
         </>
     )
 }
 export default Todo;
+
